4e4f2d7756db52ef97c4dbf3fecb4e8869f2df79
Mohit Kumar Singh <mohit_kumar.singh@rail.bombardier.com>
2020-01-16 12:15:56 +0530

    Initial commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+
+using DatingApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options): base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
